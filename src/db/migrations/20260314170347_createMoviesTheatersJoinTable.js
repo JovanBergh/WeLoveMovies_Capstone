6c9dc92bcc2eb7f2891379ea787c0a0586eb73a2
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-14 11:34:52 -0600

    added createMoviesTheatersJoinTable

---
+++ src/db/migrations/20260314170347_createMoviesTheatersJoinTable.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+
+    await knex.schema.createTable("movies_theaters", (table) => {
+    table.uuid("movie_id").notNullable();
+    table.foreign("movie_id")
+        .references("movie_id")
+        .inTable("movies")
+        .onDelete("CASCADE");
+    table.uuid("theater_id").notNullable();
+    table.foreign("theater_id")
+        .references("theater_id")
+        .inTable("theaters")
+        .onDelete("CASCADE");
+    table.boolean("is_showing").defaultTo(false).notNullable();
+    table.timestamps(true, true);
+  });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    
+  await knex.schema.dropTable("movies_theaters");
+
+};
