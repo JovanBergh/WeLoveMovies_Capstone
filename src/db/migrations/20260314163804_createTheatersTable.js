17d627d8817581507ea2ce1c4c48e0b6c29eea5a
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-14 11:03:16 -0600

    added createTheatersTable migration

---
+++ src/db/migrations/20260314163804_createTheatersTable.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async  function(knex) {
+  await knex.schema.createTable("theaters", (table) => {
+    table.uuid("theater_id").primary().defaultTo("uuid_generate_v4()");
+    table.string("name").notNullable();
+    table.string("address_line_1").notNullable();
+    table.string("address_line_2");
+    table.string("city").notNullable();
+    table.string("state").notNullable();
+    table.string("zip").notNullable();
+    table.timestamps(true, true);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    
+  await knex.schema.dropTable("theaters");
+
+};
