a88f67b6f87d0383943cbd28ec4848674758733e
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-15 10:47:09 -0600

    separated callback function is-showing to /utils

---
+++ src/utils/is-showing.js
@@ -0,0 +1,10 @@
+function isShowing(queryBuilder, is_showing) {
+  if (is_showing) {
+    queryBuilder
+      .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+      .where({ "mt.is_showing": true })
+      .groupBy("m.movie_id");
+  }
+};
+
+module.exports = isShowing;
