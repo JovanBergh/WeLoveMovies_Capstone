63efe7c72d90d5a4dfcc60d40ab845e136fe3480
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-21 18:57:04 -0600

    Restructured logger file

--- logger.js
+++ logger.js
@@ -1,22 +1,64 @@
 const pinoHttp = require("pino-http");
 const { nanoid } = require("nanoid");
 
-const level = process.env.LOG_LEVEL || "info";
 
+//CONFIGURATIONS
+const level = process.env.LOG_LEVEL || "info";
 const nodeEnv = process.env.NODE_ENV || "development";
 
+const customAttributeKeys = {
+  req: 'request',
+  res: 'response',
+};
+
+const serializers = {
+  info: {
+  req(req) {
+    return {
+      id: req.id,
+      path: req.url,
+      origin: req.remoteAddress,
+      port: req.remotePort,
+    }
+  },
+  res(res) {
+    return {
+      allowed : res.headers["access-control-allow-origin"],
+      type: res.headers["content-type"],
+    }
+  }},
+};
+
+const customSuccessMessage = {
+  info:  (req,res) => {return `\n(${res.statusCode}) ${req.method} "${req.url}"`},
+};
 
 //LOGGERS
-const logger = (level, transport, serializers) => {
+const logger = (level) => {
+
+  //Formatting for terminals
+  const transport = nodeEnv === "development" && process.stdout.isTTY ?
+    {
+      target: 'pino-pretty',
+      options: {
+        colorize: true
+      }
+    } : '' ;
+  
+  //Setting serializer
+  const setSerializer = serializers[level]; 
+  const setSuccessMessage =  customSuccessMessage[level]
+
   return pinoHttp({
-  genReqId: (request) => request.headers["x-request-id"] || nanoid(),
-  level,
-  transport,
-  serializers,
-  customSuccessMessage,
-  customAttributeKeys
+    genReqId: (request) => request.headers["x-request-id"] || nanoid(),
+    level: level,
+    transport,
+    serializers: setSerializer,
+    customSuccessMessage: setSuccessMessage,
+    customAttributeKeys
 })};
 
+/*TO DO: Separate application, auditing & security streams
 const app = (level, transport, serializers) => {
   return pinoHttp({
   genReqId: (request) => request.headers["x-request-id"] || nanoid(),
@@ -46,45 +88,7 @@ const security = (level, transport, serializers) => {
   customSuccessMessage,
   customAttributeKeys
 })};
-
-//DEV: HUMAN-READABLE OUTPUT
-const transport = 
-  process.stdout.isTTY && nodeEnv === "development"
-  ? {
-    target: 'pino-pretty',
-    options: {
-      colorize: true
-    }
-  } : '' ;
-
-const serializers = {
-  req(req) {
-    return {
-      id: req.id,
-      path: req.url,
-      origin: req.remoteAddress,
-      port: req.remotePort,
-    }
-  },
-  res(res) {
-    return {
-      allowed : res.headers["access-control-allow-origin"],
-      type: res.headers["content-type"],
-  }
-},
-
-};
-
-const customSuccessMessage = (req,res) => {
-  return `\n(${res.statusCode}) ${req.method} "${req.url}"`
-}
-
-const customAttributeKeys = {
-  req: 'request',
-  res: 'response',
-}
-
-
-module.exports = [
+*/
+module.exports = {
   logger
-];
+};
